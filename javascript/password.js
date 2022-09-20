a3df82e8f2c7629e0708f0d5159554af5aceaf7b
Fenwick-Royce <Fenwick.Royce@gmail.com>
2022-09-20 14:58:09 -0700

    updated javascript for ease of functionality

--- javascript/password.js
+++ javascript/password.js
@@ -1,22 +1,12 @@
 function enterpassword(individualpassWord){
-    if(individualpassWord.length >= 6 && individualpassWord.length <= 20){
-        let firstChar = parseInt (individualpass.charAt(6));
-        if(!isNaN(firstChar)){
-                alert("Password requires to be 6 characters long");
-        }
-        else {
-            alert("password accepted");
-        }
+    if (individualpassWord.length >=6 && individualpassWord.length <=20 && isNaN(parseInt(individualpassWord))){
+        alert ("Password accepted");
     }
-    var str1 = str.toLowerCase();
-    var str2 = str.toUpperCase();
-    var str3 = "!@#$%^&*()";
-    var str4 = 123456789 ;
+    else {
+        alert ("password rejected")
+    }
 }
-console.log(enterpassword)
-console.log(individualpassWord)
-
-function btnPrompt(){
-    let btnPrompt = prompt("enter a password")
-    pwRequirement("enter a password with 6 characters minimum")
+function typeinpassword(){
+    let passwordprompt = prompt("Please enter a password")
+    enterpassword(passwordprompt)
 }
