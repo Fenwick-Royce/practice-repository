7b7dc7257993bc1ad0b7cf5c2f111db96ce6bd22
Fenwick-Royce <Fenwick.Royce@gmail.com>
2022-09-22 12:21:12 -0700

    update

--- javascript/numbers.js
+++ javascript/numbers.js
@@ -10,3 +10,16 @@ let sandwich = {
     bread: "whole wheat"
     calories: 250
 }
+
+for(const value of sandwich) {
+    console.log(value)
+  }
+  // Error: Objects are not iterable
+  
+  for(const key in sandwich) {
+    console.log(key)
+  }
+  // "grilled"
+  // "butter"
+  // "bread"
+  // "calories"
