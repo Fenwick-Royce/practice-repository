0a809bc6800006130906fa4deb2a015329a47ad5
Fenwick-Royce <Fenwick.Royce@gmail.com>
2022-10-11 14:40:46 -0700

    tried the magic 8 ball. still need some assistance

---
+++ magic8ball/javascript.js
@@ -0,0 +1,15 @@
+let imgarray = [];
+
+let form = document.form.eightball;
+
+for(let 1 = i; i <= 15; i++) {
+    imgArray[i] = new Image();
+    imgArray[i].src = 'img/b' + i + ".jpg";
+    console.log(imgArray[i]);
+}
+
+function ask() {
+    let answer = document.getElementById(Answers);
+    console.log(num);
+    return num;
+}
